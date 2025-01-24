bc16af5fb6dcf020ba03d52505e599dc3592ed60
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2025-01-23 23:45:52 -0500

    week-10 assignment

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
@@ -7,19 +13,30 @@ const authUser = require("./middleware/authentication");
 
 //routers
 const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
+// const jobsRouter = require("./routes/jobs");
 const todosRouter = require("./routes/todos");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authUser, jobsRouter);
+// app.use("/api/v1/jobs", authUser, jobsRouter);
 app.use("/api/v1/todos", authUser, todosRouter);
 
 app.use(notFoundMiddleware);
--- controllers/todos.js
+++ controllers/todos.js
@@ -2,23 +2,62 @@ const ToDo = require("../models/ToDo");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
+// GET ALL TODOS
 const getAllToDos = async (req, res) => {
-  res.send("get all jobs");
+  const todos = await ToDo.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).json({ todos, count: todos.length });
 };
-
+// GET SINGLE TODO
 const getToDo = async (req, res) => {
-  res.send("get all todos");
+  const {
+    user: { userId },
+    params: { id: todoId },
+  } = req;
+  const todo = await ToDo.findOne({ _id: todoId, createdBy: userId });
+
+  if (!todo) {
+    throw new NotFoundError(`No todo found with id: ${todoId}`);
+  }
+  res.status(StatusCodes.OK).json({ todo });
 };
+// CREATE TODO
 const createToDo = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const todo = await ToDo.create(req.body);
   res.status(StatusCodes.CREATED).json({ todo });
 };
+//UPDATE TODO
 const updateToDo = async (req, res) => {
-  res.send("get all todos");
+  const {
+    body: { title, description },
+    user: { userId },
+    params: { id: todoId },
+  } = req;
+  if (!title || !description) {
+    throw new BadRequestError("Title or Description fields cannot be empty");
+  }
+  const todo = await ToDo.findByIdAndUpdate(
+    { _id: todoId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!todo) {
+    throw new NotFoundError(`No todo task found with id: ${todoId}`);
+  }
+  res.status(StatusCodes.OK).json({ todo });
 };
 const deleteToDo = async (req, res) => {
-  res.send("get all todos");
+  const {
+    user: { userId },
+    params: { id: todoId },
+  } = req;
+  const todo = await ToDo.findOneAndRemove({ _id: todoId, createdBy: userId });
+  if (!todo) {
+    throw new NotFoundError(`No todo task found with id: ${todoId}`);
+  }
+  res.status(StatusCodes.NO_CONTENT).send();
 };
 
 module.exports = { getAllToDos, getToDo, createToDo, updateToDo, deleteToDo };

d6417482343835a27ff8cfc6dbc85b74a2bdb15d
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2025-01-25 00:28:20 -0500

    swagger docs added

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
@@ -33,6 +38,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send(`<h1> ToDo API </h1><a href="/api-docs">Documentation</a>`);
+});
+
+// swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
